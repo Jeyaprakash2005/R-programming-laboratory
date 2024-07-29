db780080527452ef6b22075d3b67aa7230cffe8c
Jeya prakash J <160204019+Jeyaprakash2005@users.noreply.github.com>
2024-07-29 15:30:58 +0530

    Add files via upload

---
+++ exp 1 prg 22AI020.R
@@ -0,0 +1,38 @@
+install.packages("ggplot2")
+library(ggplot2)
+help(package="ggplot2")
+x<-3
+x
+8+2
+3-2
+3*3
+4/2
+42%/%4
+x<-10
+abs(x)
+sqrt(x)
+exp(x)
+exists("x")
+cos(x)
+
+a <- c (1, 0.1)
+b <- c (2.33, 4)
+print (a+b)
+
+B= c(4,4) 
+C= c(5,5)
+print (B*C)
+
+list1 <- c(TRUE, 0.1)
+list2 <- c(0,4+3i)
+print(list1|list2)
+
+list1 <- c(TRUE, 0.1,"apple")
+list2 <- c(0,0.1,"bat")
+print(list1<list2)
+
+list1<- c(2, 22)
+list2<-c(2,4)
+print(list1 %% list2)
+
+4^3
